37acef89d8f8509fcbb6bcca9ba42dfcccfbd461
Fabio Dos Santos <fabio2st@yahoo.com>
2019-07-23 21:44:18 -0300

    10.Multiplo Cinco Ok

--- EstructurasControlFlujo/10.MultiplosCinco/Program.vb
+++ EstructurasControlFlujo/10.MultiplosCinco/Program.vb
@@ -11,22 +11,56 @@ Module Program
         Dim primero, segundo, primerMultiplo As UInteger
         primero = IngresarValor("Ingrese un valor")
         segundo = IngresarValor("Ingrese un valor mayor")
-        primerMultiplo = primero + valorMultiplo - (primero Mod valorMultiplo)
-
-        resolverConFor(primero, segundo, primerMultiplo)
+        primerMultiplo = ObetenerPrimerMultiplo(primero)
         resolverConDoWhile(primero, segundo, primerMultiplo)
+        resolverConDoUntil(primero, segundo, primerMultiplo)
+        resolverConLoopWhile(primero, segundo, primerMultiplo)
+        resolverConLoopUntil(primero, segundo, primerMultiplo)
+        resolverConFor(primero, segundo, primerMultiplo)
     End Sub
-
+    Private Function ObetenerPrimerMultiplo(primero As UInteger) As UInteger
+        If primero Mod valorMultiplo = 0 Then
+            Return primero
+        Else
+            Return primero + valorMultiplo - (primero Mod valorMultiplo)
+        End If
+    End Function
     Private Sub resolverConDoWhile(primero As UInteger, segundo As UInteger, primerMultiplo As UInteger)
         Dim cantidad As UInteger
         Dim x As UInteger = primerMultiplo
-        Do While x < segundo
+        Do While x <= segundo
             cantidad += 1
             x += valorMultiplo
         Loop
         mostrarResultado("Do While", primero, segundo, cantidad)
     End Sub
-
+    Private Sub resolverConDoUntil(primero As UInteger, segundo As UInteger, primerMultiplo As UInteger)
+        Dim cantidad As Integer = 0
+        Dim x As Integer = primerMultiplo
+        Do Until x > segundo
+            cantidad += 1
+            x += valorMultiplo
+        Loop
+        mostrarResultado("Do Until", primero, segundo, cantidad)
+    End Sub
+    Private Sub resolverConLoopWhile(primero As UInteger, segundo As UInteger, primerMultiplo As UInteger)
+        Dim cantidad As Integer = -1
+        Dim x As Integer = primerMultiplo - valorMultiplo
+        Do
+            cantidad += 1
+            x += valorMultiplo
+        Loop While x <= segundo
+        mostrarResultado("Loop While", primero, segundo, cantidad)
+    End Sub
+    Private Sub resolverConLoopUntil(primero As UInteger, segundo As UInteger, primerMultiplo As UInteger)
+        Dim cantidad As Integer = -1
+        Dim x As Integer = primerMultiplo - valorMultiplo
+        Do
+            cantidad += 1
+            x += valorMultiplo
+        Loop Until x > segundo
+        mostrarResultado("Loop Until", primero, segundo, cantidad)
+    End Sub
     Private Sub resolverConFor(primero As UInteger, segundo As UInteger, primerMultiplo As UInteger)
         Dim cantidad As UInteger
         For x = primerMultiplo To segundo Step valorMultiplo
@@ -34,14 +68,11 @@ Module Program
         Next
         mostrarResultado("For", primero, segundo, cantidad)
     End Sub
-
     Private Sub mostrarResultado(mensaje As String, primero As UInteger, segundo As UInteger, cantidad As UInteger)
         Console.WriteLine("Cantidad de múltiplos de {0} entre {1} y {2} son {3} con {4}", valorMultiplo, primero, segundo, cantidad, mensaje)
     End Sub
-
     Private Function IngresarValor(mensaje As String) As UInteger
         Console.Write(mensaje & ": ")
         Return Console.ReadLine()
     End Function
-
 End Module
