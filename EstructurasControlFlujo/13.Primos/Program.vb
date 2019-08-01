f662b316af794e6211392814ed74224a6a5b5101
Fabio Dos Santos <fabio2st@yahoo.com>
2019-08-01 20:33:49 -0300

    13.Primos

--- EstructurasControlFlujo/13.Primos/Program.vb
+++ EstructurasControlFlujo/13.Primos/Program.vb
@@ -5,31 +5,30 @@ Module Program
         Console.WriteLine("Un número primo es un número natural mayor que 1 que tiene únicamente dos divisores distintos: él mismo y el 1")
         Dim valor As UInteger = 2
         For x = 1 To 20
-            ObtenerProximoPrimo(2)
+            ObtenerProximoPrimo(valor)
+            Console.Write(" " & valor)
         Next
     End Sub
-
-    Sub ObtenerProximoPrimo(valor As UInteger)
+    Sub ObtenerProximoPrimo(ByRef valor As UInteger)
+        Do
+            valor += 1
+        Loop Until isPrimo(valor)
+    End Sub
+    Function isPrimo(valor As UInteger) As Boolean
         Dim divisor, raizcuadrada As UInteger
         ' el máximo valor divisor sera equivalente a la raíz cuadrada del valor
         ' esto reduce iteraciones innecesarias
         raizcuadrada = Math.Sqrt(valor)
-        Console.WriteLine("Raiz cuadrada: " & raizcuadrada)
+        'Console.WriteLine("Raiz cuadrada: " & raizcuadrada)
         ' el primer valor divisor para descartar un numero primo es 2
         divisor = 2
-
         ' itera si el resto es distinto de cero y el divisor es menor o igual a la raíz cuadrada
         ' si el primer calculo de resto da 0 no itera en absoluto
         Do While valor Mod divisor <> 0 And divisor <= raizcuadrada
-            Console.Write(" " & divisor)
+            'Console.Write(" " & divisor)
             divisor += 1
         Loop
-
         'si el divisor es mayor a la raíz cuadrada es primo
-        If divisor > raizcuadrada Then
-            Console.WriteLine(vbCrLf & "Es primo")
-        Else
-            Console.WriteLine(vbCrLf & "No es primo")
-        End If
-    End Sub
+        Return divisor > raizcuadrada
+    End Function
 End Module
